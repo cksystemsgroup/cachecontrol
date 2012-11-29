b1f27b68d6cfbe501b560ac5d5b5868ddfd9fd41
Martin Aigner <martin@maigner.net>
2012-11-29 17:11:33 +0100

    -read cr0 register

--- cachecontrol.c
+++ cachecontrol.c
@@ -3,9 +3,27 @@
 
 MODULE_LICENSE("Dual BSD/GPL");
 
-static int cachecontrol_init(void) {
-	printk(KERN_ALERT "Disable caches\n");
+static inline void cr0_status(void) {
 
+	long cr0;
+
+	__asm__(
+			"mov %%cr0, %0;"
+			:"=r"(cr0)
+			: /* no input registers */
+			: /* no explicit clobbered regs */
+	       );
+
+	printk(KERN_DEBUG "cr0: %02lX\n", cr0);
+}
+
+
+
+static int cachecontrol_init(void) {
+
+	cr0_status();
+
+	printk(KERN_NOTICE "Disabling caches...\n");
 	__asm__(
 			"push %rax\n\t"
 			"mov %cr0,%rax\n\t"
@@ -15,18 +33,26 @@ static int cachecontrol_init(void) {
 			"pop %rax\n\t");
 			
 
+	printk(KERN_NOTICE "Disabling caches...DONE\n");
+	
+	cr0_status();
 	return 0;
 }
 static void cachecontrol_exit(void) {
-	printk(KERN_ALERT "Enable caches\n");
-
+	
+	cr0_status();
+	
+	printk(KERN_NOTICE "Enabling caches...\n");
+	
 	__asm__(
 			"push %rax\n\t"
 			"mov %cr0,%rax\n\t"
-			"and $~(1<<30),%rax\n\t"
+			"and $(~(1<<30)),%rax\n\t"
 			"mov %rax,%cr0\n\t"
 			"pop %rax\n\t");
-			
+
+	printk(KERN_NOTICE "Enabling caches...DONE\n");
+	cr0_status();		
 }
 
 module_init(cachecontrol_init);
